1bdb34090af79fac727482794b5197d36cf5fc27
plengqui <per@lengquist.se>
2016-11-02 20:45:39 +0100

    Purge dirq every 2 minutes, to clean up empty directories.

--- tmcontroller.py
+++ tmcontroller.py
@@ -1,4 +1,4 @@
-from datetime  import datetime
+from datetime  import datetime, timedelta
 from tmparser import *
 from siparser import *
 from time import sleep
@@ -26,28 +26,32 @@ class TinymeshController(object):
             punch = siparser.SiPacket.parse(buf)
         except:
             # except (construct.core.ConstructError, construct.core.FieldError, construct.core.RangeError) as e:
-            print("Could not parse packet: ", buf)
+            print("Could not parse packet: ", data)
         else:
             print(punch)
             #TODO send with SIRAP to OLA
             self.serialData.append("Control=" + punch.Cn + " Card=" + punch.SiNr + " Time=" + ThTl.strftime("%H:%M:%S"))
 
     def get_serial_data(self):
+        #this gets called by the gui to see if any new serial data has been received
         if self.serialData:
             return self.serialData.popleft()
         else:
             return None
 
-    #TODO: pick up serial data from sportident parser to send to the radio with given id
+    #TODO: pick up serial data to send to the radio with given id (for example an acknowledge)
     #TBD: how to send to a given SRR sportident station??
 
     def __init__(self):
         self.dirq = MyQueue(subject=MyQueue.SUBJECT_NETWORKPACKETS_IN)
         self.radioStatus = {}  # keep a dictionary of dictionaries
         self.serialData = deque([])
+        self.last_purge = datetime.now()
 
 
     def process_new_data(self):
+        #Check incoming queue for any new packets. Parse them with TM format.
+        # Update the radio status with RSSI etc. If serial packet, process the serial data.
         for name in self.dirq:
             if not self.dirq.lock(name):
                 continue
@@ -80,7 +84,7 @@ class TinymeshController(object):
                         }
                 #todo: it seems like i am creating a new dictionary, instead of just updating the values that are new
                 #tried fixing it by using update() instead. See how that works.
-        #        print(d.OriginId,d.MessageCounter, d.PacketType, d.ReceivedTime, d.OriginRssi, sep="\t",end="")
+                #print(d.OriginId,d.MessageCounter, d.PacketType, d.ReceivedTime, d.OriginRssi, sep="\t",end="")
                 if(d.PacketType=="ReceiveEvent" and d.PacketContents.MessageDetail in ["DigitalInputChangeDetected","Analogue0InputTrig","Analogue1InputTrig","RfJammingDetected","DeviceReset",
                                       "StatusIma", "ChannelBusySimilarId", "ChannelIsFree", "ChannelIsJammed", "OtherTmActiveOnChannel","StatusNid", "StatusNextReceiver"]):
                     self.radioStatus[d.OriginId].update({
@@ -95,4 +99,7 @@ class TinymeshController(object):
                 if(d.PacketType=="ReceiveSerial"):
                     self.process_serial_data(d.PacketContents.SerialData)
             self.dirq.remove(name)
+            if(datetime.now() - self.last_purge > timedelta(minutes=1)):
+                self.dirq.purge() #clean old debris from the queue every 2 minutes
+                self.last_purge=datetime.now()
         return self.radioStatus
